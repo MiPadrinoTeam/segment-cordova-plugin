dad80a410a25fdb0bf4bd09f17c32bb034b0b388
Alex Tanton <tantona@gmail.com>
2020-02-26 14:53:36 -0600

    try using frameworks?

--- src/ios/SegmentCordovaPlugin.m
+++ src/ios/SegmentCordovaPlugin.m
@@ -1,5 +1,7 @@
 #import "SegmentCordovaPlugin.h"
 
+#import <Segment-UrbanAirship/SEGUrbanAirshipIntegrationFactory.h>
+
 @implementation SegmentCordovaPlugin
 
 - (void)pluginInitialize {
@@ -75,7 +77,7 @@
         }
 
         if ([configOptions objectForKey:@"enableAirshipIntegration"] != nil) {
-          [configuration use:[SEGUrbanAirshipIntegrationFactory instance]];
+          [config use:[SEGUrbanAirshipIntegrationFactory instance]];
         }
 
         // if ([configOptions objectForKey:@"enableAppsFlyerIntegration"] !=
