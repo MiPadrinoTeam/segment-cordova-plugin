05930a4d838b8717cdcbb38c67e58bdad753e09f
Alex Tanton <tantona@gmail.com>
2020-03-04 12:08:29 -0600

    add logs to airship integration

--- src/ios/airship/SEGUrbanAirshipIntegration.m
+++ src/ios/airship/SEGUrbanAirshipIntegration.m
@@ -1,9 +1,8 @@
 /* Copyright Airship and Contributors */
 
 #import "SEGUrbanAirshipIntegration.h"
-#import "SEGUrbanAirshipAutopilot.h"
 #import "AirshipLib.h"
-
+#import "SEGUrbanAirshipAutopilot.h"
 
 #define kUrbanAirshipAppKey @"appKey"
 #define kUrbanAirshipAppSecret @"appSecret"
@@ -14,110 +13,121 @@
 @implementation SEGUrbanAirshipIntegration
 
 - (instancetype)initWithSettings:(NSDictionary *)settings {
-    if (self = [super init]) {
-        self.settings = settings;
-        [SEGUrbanAirshipAutopilot takeOff:settings storeConfig:YES];
-    }
+  if (self = [super init]) {
+    self.settings = settings;
+    [SEGUrbanAirshipAutopilot takeOff:settings storeConfig:YES];
+  }
 
-    return self;
+  return self;
 }
 
 - (void)identify:(SEGIdentifyPayload *)payload {
-    [UAirship namedUser].identifier = payload.userId;
+  [UAirship namedUser].identifier = payload.userId;
 
-    NSMutableArray *addTags = [NSMutableArray array];
-    NSMutableArray *removeTags = [NSMutableArray array];
+  NSMutableArray *addTags = [NSMutableArray array];
+  NSMutableArray *removeTags = [NSMutableArray array];
 
-    for (NSString *key in payload.traits) {
-        id value = payload.traits[key];
-        if (![value isKindOfClass:[NSNumber class]]) {
-            continue;
-        }
-
-        CFTypeID type = CFGetTypeID((__bridge CFTypeRef)(value));
-        if (type != CFBooleanGetTypeID()) {
-            continue;
-        }
-
-        if ([value boolValue]) {
-            [addTags addObject:key];
-        } else {
-            [removeTags addObject:key];
-        }
+  for (NSString *key in payload.traits) {
+    id value = payload.traits[key];
+    if (![value isKindOfClass:[NSNumber class]]) {
+      continue;
     }
 
-    [[UAirship namedUser] addTags:addTags group:@"segment-integration"];
-    [[UAirship namedUser] removeTags:removeTags group:@"segment-integration"];
-    [[UAirship namedUser] updateTags];
+    CFTypeID type = CFGetTypeID((__bridge CFTypeRef)(value));
+    if (type != CFBooleanGetTypeID()) {
+      continue;
+    }
+
+    if ([value boolValue]) {
+      [addTags addObject:key];
+      SEGLog(@"addTags:%@]", key);
+    } else {
+      [removeTags addObject:key];
+      SEGLog(@"removeTags:%@]", key);
+    }
+  }
+
+  [[UAirship namedUser] addTags:addTags group:@"segment-integration"];
+  [[UAirship namedUser] removeTags:removeTags group:@"segment-integration"];
+  [[UAirship namedUser] updateTags];
 }
 
 - (void)screen:(SEGScreenPayload *)payload {
-    [[[UAirship shared] analytics] trackScreen:(payload.name ?: payload.category)];
+  [[[UAirship shared] analytics]
+      trackScreen:(payload.name ?: payload.category)];
 }
 
 - (void)track:(SEGTrackPayload *)payload {
-    UACustomEvent *customEvent = [UACustomEvent eventWithName:payload.event];
-    customEvent.transactionID = @"cdp";
+  UACustomEvent *customEvent = [UACustomEvent eventWithName:payload.event];
+  customEvent.transactionID = @"cdp";
 
-    NSNumber *value = [SEGUrbanAirshipIntegration extractRevenue:payload.properties withKey:@"revenue"];
-    if (!value) {
-        value = [SEGUrbanAirshipIntegration extractRevenue:payload.properties withKey:@"value"];
+  NSNumber *value =
+      [SEGUrbanAirshipIntegration extractRevenue:payload.properties
+                                         withKey:@"revenue"];
+  if (!value) {
+    value = [SEGUrbanAirshipIntegration extractRevenue:payload.properties
+                                               withKey:@"value"];
+  }
+
+  if (value) {
+    customEvent.eventValue =
+        [NSDecimalNumber decimalNumberWithDecimal:[value decimalValue]];
+  }
+
+  for (NSString *key in payload.properties) {
+    id value = payload.properties[key];
+
+    if ([value isKindOfClass:[NSString class]]) {
+      [customEvent setStringProperty:value forKey:key];
+      SEGLog(@"customEvent:%@]", key);
     }
 
-    if (value) {
-        customEvent.eventValue = [NSDecimalNumber decimalNumberWithDecimal:[value decimalValue]];
+    if ([value isKindOfClass:[NSNumber class]]) {
+      [customEvent setNumberProperty:value forKey:key];
+      SEGLog(@"customEvent:%@]", key);
     }
+  }
 
-    for (NSString *key in payload.properties) {
-        id value = payload.properties[key];
-
-        if ([value isKindOfClass:[NSString class]]) {
-            [customEvent setStringProperty:value forKey:key];
-        }
-
-        if ([value isKindOfClass:[NSNumber class]]) {
-            [customEvent setNumberProperty:value forKey:key];
-        }
-    }
-
-    [[UAirship shared].analytics addEvent:customEvent];
+  [[UAirship shared].analytics addEvent:customEvent];
 }
 
 - (void)group:(SEGGroupPayload *)payload {
-    if (payload.groupId) {
-        [[UAirship push] addTag:payload.groupId];
-        [[UAirship push] updateRegistration];
-    }
-}
-
-// Reset is invoked when the user logs out, and any data saved about the user should be cleared.
-- (void)reset {
-    [UAirship namedUser].identifier = nil;
-    [UAirship push].tags = @[];
+  if (payload.groupId) {
+    [[UAirship push] addTag:payload.groupId];
     [[UAirship push] updateRegistration];
+  }
 }
 
-+ (NSNumber *)extractRevenue:(NSDictionary *)dictionary withKey:(NSString *)revenueKey {
-    id revenueProperty;
+// Reset is invoked when the user logs out, and any data saved about the user
+// should be cleared.
+- (void)reset {
+  [UAirship namedUser].identifier = nil;
+  [UAirship push].tags = @[];
+  [[UAirship push] updateRegistration];
+}
 
-    for (NSString *key in dictionary) {
-        if ([key caseInsensitiveCompare:revenueKey] == NSOrderedSame) {
-            revenueProperty = dictionary[key];
-            break;
-        }
-    }
++ (NSNumber *)extractRevenue:(NSDictionary *)dictionary
+                     withKey:(NSString *)revenueKey {
+  id revenueProperty;
 
-    if (revenueProperty) {
-        if ([revenueProperty isKindOfClass:[NSString class]]) {
-            // Format the revenue.
-            NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-            [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
-            return [formatter numberFromString:revenueProperty];
-        } else if ([revenueProperty isKindOfClass:[NSNumber class]]) {
-            return revenueProperty;
-        }
+  for (NSString *key in dictionary) {
+    if ([key caseInsensitiveCompare:revenueKey] == NSOrderedSame) {
+      revenueProperty = dictionary[key];
+      break;
     }
-    return nil;
+  }
+
+  if (revenueProperty) {
+    if ([revenueProperty isKindOfClass:[NSString class]]) {
+      // Format the revenue.
+      NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+      [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+      return [formatter numberFromString:revenueProperty];
+    } else if ([revenueProperty isKindOfClass:[NSNumber class]]) {
+      return revenueProperty;
+    }
+  }
+  return nil;
 }
 
 @end
